9346677e267a683b06b46b99f3dd66e820ceeddc
SebastianTovar64 <sebastovaes@hotmail.com>
2023-09-18 07:24:18 -0500

    Crudphp
    Crudphp

---
+++ prueba_php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$conexion = new mysqli('localhost', 'root','', 'prueba_datos');
+if ($conexion->connect_errno){
+    die ('Lo siento hubo un problema en la conexion');
+}else{
+    $sql ="INSERT INTO usuarios (ID, nombre, edad) VALUES (null, 'sonia',30)";
+    $resultado = $conexion->query($sql);
+    if($conexion->affected_rows >=1){
+        echo 'Filas agredas: ' . $conexion->affected_rows;
+    }
+}
+?>
+
